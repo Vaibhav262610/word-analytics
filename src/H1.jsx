09e9fa8afa8bb5bf34ec1958e4aa5cd04a95a361
Vaibhav262610 <branda56235@gmail.com>
2024-11-23 18:41:56 +0530

    H

--- src/H1.jsx
+++ src/H1.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function H1() {
   return (
     <div>
-      <h1 className='first-heading '>Word <span className='first-heading--thin'>Analytics</span></h1>
+      <h1 className='first-heading '>Word<span className='first-heading--thin'>Analytics</span></h1>
     </div>
   )
 }
